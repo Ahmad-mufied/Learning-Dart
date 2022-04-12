354d7d91e0e7d127b1964f976b986bbcd8b70a39
ahmad-mufied <teranggelap01@gmail.com>
2022-04-12 08:22:07 +0700

    add constanClass

--- dartOpp/singleton.dart
+++ dartOpp/singleton.dart
@@ -4,8 +4,18 @@ class Singleton {
   factory Singleton() => _instance;
 }
 
+class ConstantClass {
+  const ConstantClass();
+}
+
 void main() {
   var s1 = Singleton();
   var s2 = Singleton();
+
+  ConstantClass c1 = const ConstantClass();
+  ConstantClass c2 = const ConstantClass();
+
   print('identical(s1, s2) --> ${identical(s1, s2)}');
+
+  print('identical(c1, c2) --> ${identical(c1, c2)}');
 }
